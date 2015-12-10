e58cd391d29197a042390bcad2ef4f8087769b95
xyzsolovers <shun@shun-ichiro-no-MacBook-Air.local>
2015-12-10 17:39:56 +0900

    added create action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,51 @@
 class ProjectsController < ApplicationController
+
+      def index
+      	  @articles = Article.all
+      end
+
+      def new
+      	  @article = Article.new
+      end	  
+
+
+      def create
+      	  @article = Article.new(article_params)
+	  respond_to do |format|
+	    if @article.save
+	      format.html{redirect_to action:'index', notice:'Article was successfully created'}
+	      format.json{render :show, status: :created, location: @article}
+	    else
+	      format.html{render action: 'new'}
+	      format.json{render json: @book.errors, status: unprocessable_entity}   
+	    end
+	  end  
+      end	  
+
+      def update
+      	  respond_to do |format|
+      	  	     if @article.update(article_params)
+        	     format.html { redirect_to @article, notice: 'Article was successfully updated\
+.' }
+			format.json { render :show, status: :ok, location: @article }
+      			else
+        		format.html { render :edit }
+        		format.json { render json: @article.errors, status: :unprocessable_entity \
+}
+	   end
+    	end
+     end
+
+
+
+
+
+
+ private
+     def article_params
+     	 params.require(:article).permit(:article_text)
+     end
+
+
+
 end
--- db/migrate/20151210024218_create_articles.rb
+++ db/migrate/20151210024218_create_articles.rb
@@ -1,7 +1,9 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-
+    
+      t.text :article_text
+      	
       t.timestamps null: false
     end
   end
