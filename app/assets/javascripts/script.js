b063c71ce019870830e6496f3b49b4f5c0c639b2
xyzsolovers <shun@shun-ichiro-no-MacBook-Air.local>
2015-12-10 11:43:58 +0900

    made new.html.erb

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,30 @@
+
+       
+	   
+
+
+
+$(function(){
+      
+	   
+
+        $('.MdWidgetList li').click(function(){
+
+                var index = $('.MdWidgetList li').index(this);
+
+                $('.Widgetcontents li').css('display','none');
+
+                $('.Widgetcontents li').eq(index).css('display','block');
+
+                $('.MdWidgetList li').removeClass('select');
+
+                $(this).addClass('select');
+
+
+            });
+
+
+
+
+
+    });
